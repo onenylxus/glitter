f2b0b223fede71884a5845ae05191b30f89da651
Nicholas Ng <onenylxus@gmail.com>
2024-01-21 21:39:34 +0800

    Update logo design

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
-        tint: 'bg-tint text-[#f9fafd] shadow hover:bg-tint/90',
+        tint: 'bg-tint text-[#fafcff] shadow hover:bg-tint/90',
       },
       size: {
         default: 'h-9 px-4 py-2',
