179fcdcfdd318b5219f22d1cf74a1847a55e69a3
Nicholas Ng <onenylxus@gmail.com>
2024-02-03 21:21:17 +0800

    Update notes page when without notes

--- app/(main)/(routes)/notes/page.jsx
+++ app/(main)/(routes)/notes/page.jsx
@@ -1,6 +1,22 @@
+'use client'
+
+import { Button } from '@/components/ui/button';
+import { PlusCircle } from 'lucide-react';
+import { useUser } from '@clerk/clerk-react';
+import Image from 'next/image';
+
 const NotesPage = () => {
+  const { user } = useUser();
+
   return (
-    <div>Notes page</div>
+    <div className="flex h-full space-y-4 flex-col justify-center items-center">
+      <Image src="undraw/add_notes.svg" width="300" height="300" alt="Add Notes" />
+      <h2 className="font-medium text-lg">Welcome back, {user.firstName}.</h2>
+      <Button variant="tint">
+        <PlusCircle className="w-4 h-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
   );
 };
 
