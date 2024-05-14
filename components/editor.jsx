b3040de118249d11c8eac4d4da37d408ae464454
Nicholas Ng <onenylxus@gmail.com>
2024-05-14 11:43:56 +0800

    Editor component

---
+++ components/editor.jsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { BlockNoteView } from '@blocknote/mantine';
+import { useCreateBlockNote } from '@blocknote/react';
+import { useEdgeStore } from '@/lib/edgestore';
+import { useTheme } from 'next-themes';
+import '@blocknote/mantine/style.css';
+
+export const Editor = ({ data, onChange, editable = true }) => {
+  const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
+
+  const handleUpload = async (file) => {
+    const res = await edgestore.publicFiles.upload({ file });
+    return res.url;
+  };
+
+  const editor = useCreateBlockNote({
+    initialContent: data ? JSON.parse(data) : undefined,
+    uploadFile: handleUpload,
+  });
+
+  const onEdit = () => {
+    onChange(JSON.stringify(editor.document, null, 2));
+  };
+
+  return (
+    <div>
+      <BlockNoteView
+        editor={editor}
+        theme={resolvedTheme}
+        editable={editable}
+        onChange={onEdit}
+      />
+    </div>
+  );
+};
